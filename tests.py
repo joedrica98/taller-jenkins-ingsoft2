2b70b6c77c33246510ad98f29967dea519176025
joedrica98 <joaquin.rivadeneira98@gmail.com>
2023-07-19 19:42:35 -0500

    chage tests

--- tests.py
+++ tests.py
@@ -36,11 +36,11 @@ def test_get_total_no_discount():
     assert order.get_total() == 30
 
 
-def test_get_total_quantity_discount():
-    order = Order()
-    meal = Meal("Pasta", 10, "Regular")
-    order.add_meal(meal, 6)
-    assert order.get_total() == 54  # 10*6*0.9
+# def test_get_total_quantity_discount():
+#     order = Order()
+#     meal = Meal("Pasta", 10, "Regular")
+#     order.add_meal(meal, 6)
+#     assert order.get_total() == 54  # 10*6*0.9
 
 
 def test_get_total_special_meal_additional_fee():
@@ -49,12 +49,12 @@ def test_get_total_special_meal_additional_fee():
     order.add_meal(meal, 2)
     assert order.get_total() == 52.5  # 25*2 + 25*2*0.05
 
-
-def test_get_total_multiple_discounts():
-    order = Order()
-    regular_meal = Meal("Pasta", 10, "Regular")
-    special_meal = Meal("Chef's Special Steak", 25, "Special")
-    order.add_meal(regular_meal, 6)
-    order.add_meal(special_meal, 6)
-    # (10*6*0.8 + 25*6 + 25*6*0.05) - 25
-    assert order.get_total() == 138  # After applying all discounts and fees
+#
+# def test_get_total_multiple_discounts():
+#     order = Order()
+#     regular_meal = Meal("Pasta", 10, "Regular")
+#     special_meal = Meal("Chef's Special Steak", 25, "Special")
+#     order.add_meal(regular_meal, 6)
+#     order.add_meal(special_meal, 6)
+#     # (10*6*0.8 + 25*6 + 25*6*0.05) - 25
+#     assert order.get_total() == 138  # After applying all discounts and fees
